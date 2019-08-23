7e26a0cc698ed42e8bbb8b04177d972bf4056969
JB Tellez <jb.tellez@gmail.com>
2019-08-23 14:03:42 -0700

    Step 2 models (#2)
    * adds skeleton project
    
    * adds Game model
    
    * adds Cell model
    
    * migrates cell

---
+++ games/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Game(models.Model):
+    flight = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'Flight: {self.flight}'
+
+class Cell(models.Model):
+    type = models.CharField(max_length=64, default='wall')
+    movement_cost = models.IntegerField(default=0)
+    x_pos = models.IntegerField()
+    y_pos = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.type} : {self.x_pos},{self.y_pos}'
+
