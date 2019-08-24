1d120a31b8260bcaf6a11d64736185174598fa48
JB <jb.tellez@gmail.com>
2019-08-23 19:02:48 -0700

    adds intentional bug

--- api/tests.py
+++ api/tests.py
@@ -3,5 +3,4 @@ from django.test import TestCase
 # Create your tests here.
 class ApiTests(TestCase):
     def test_fail(self):
-        self.assertTrue(False)
         self.assertEqual(True, False)
