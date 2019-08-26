a312eac1090035243ec15c49b9472577340ccea6
JB <jb.tellez@gmail.com>
2019-08-26 10:39:15 -0700

    passes tests for circle ci

--- api/tests.py
+++ api/tests.py
@@ -2,6 +2,5 @@ from django.test import TestCase
 
 # Create your tests here.
 class ApiTests(TestCase):
-    def test_fail(self):
-        self.assertEqual(True, False)
+    def test_pass(self):
         self.assertEqual(True, True)
